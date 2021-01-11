6eda163f69d4c9da2016af86506bc6438f2b2b31
Pepe Lluyot <pepe.lluyot@iescristobaldemonroy.es>
2021-01-11 11:46:33 +0100

    scan ble

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import {BLE} from '@ionic-native/ble/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  devices:any[] = [];
+  constructor(private ble: BLE, private ngZone: NgZone) {}
+  
+  Scan(){
+    this.devices=[];
+    this.ble.scan([],15).subscribe(device => this.onDeviceDiscovered(device));
+  }
+  
+  onDeviceDiscovered(device){
+    console.log('Discovered' + JSON.stringify(device,null,2));
+    this.ngZone.run(()=>{
+      this.devices.push(device)
+      console.log(device)
+    })
+  }
 
-  constructor() {}
 
 }
